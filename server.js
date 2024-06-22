e0b2f4785e92c8e42de8e38a38b6e6224e748583
XaCourse <courses4xa@gmail.com>
2024-06-22 12:15:49 +0530

    update 1

--- server.js
+++ server.js
@@ -4,19 +4,12 @@ const app = express()
 
 const bodyParse = require('body-parser')
 app.use(bodyParse.json())
-const Person = require('./model/Person')
 
 require('dotenv').config();
 const port = process.env.PORT || 3000
 
 app.get("/", function(req, res){
     res.status(200).json({message:"welcome to my page"})
-})
-
-
-app.get("/Hii", async(req, res) =>{
-    var data = await Person.find()
-    res.status(200).json({status:"success"})
 })
 
 // import and use person routes
